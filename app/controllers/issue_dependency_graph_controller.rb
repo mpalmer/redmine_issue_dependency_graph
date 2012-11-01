dec9f5b15f78175d72a6ad31473628dc45b3fc07
Matt Palmer <mpalmer@hezmatt.org>
2012-11-01 16:13:46 +1100

    Change the direction of the arrow on some relation types
    I want to view the 'flow' of issues in a 'do the bottom first' style.  This
    unfortunately requires the arrows to point 'backwards' for some types of
    relations, because graphviz always draws arrows pointing down when it
    can.  Oh well.

--- app/controllers/issue_dependency_graph_controller.rb
+++ app/controllers/issue_dependency_graph_controller.rb
@@ -44,14 +44,13 @@ class IssueDependencyGraphController < ApplicationController
 				# http://www.redmine.org/projects/redmine/wiki/Rest_IssueRelations
 				# sez that relations can either be 'relates', 'blocks', or 'precedes'
 				# I add 'child', because that's how I roll.
-				style = case ir[:type]
-					when 'blocks'   then '[style=solid,  color=red]'
-					when 'precedes' then '[style=solid,  color=black]'
-					when 'relates'  then '[style=dotted, color=black]'
-					when 'child'    then '[style=dashed, color=grey]'
-					else '[style=bold, color=pink]'
+				io.puts case ir[:type]
+					when 'blocks'   then "#{ir[:to]} -> #{ir[:from]} [style=solid,  color=red]"
+					when 'precedes' then "#{ir[:to]} -> #{ir[:from]} [style=solid,  color=black]"
+					when 'relates'  then "#{ir[:from]} -> #{ir[:to]} [style=dotted, color=black]"
+					when 'child'    then "#{ir[:from]} -> #{ir[:to]} [style=dashed, color=grey]"
+					else "#{ir[:from]} -> #{ir[:to]} [style=bold, color=pink]"
 				end
-				io.puts "#{ir[:from]} -> #{ir[:to]} #{style}"
 			end
 
 			io.puts "}"
